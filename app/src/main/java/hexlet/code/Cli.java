1564e42b5948c3f09e69a8940407cffd76e87679
Almaz Salyakhov <fpsuip@gmail.com>
2022-10-05 08:18:10 +0300

    fix Убрал лишний перенос строк

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,12 +6,10 @@ class Cli {
     private static Scanner sc = new Scanner(System.in);
 
     public static void greeting() {
-        System.out.println();
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = sc.nextLine();
         Engine.setGamerName(name);
         System.out.print("Hello, " + name + "!\n");
-
     }
 }
